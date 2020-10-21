f3d6fd749713cc1a5bc77fe0e46369407f1e2fca
aki <aki.egfree@gmail.com>
2020-10-21 03:34:39 +0000

    add update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,6 +25,22 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    
+    if @user.update(user_params)
+      flash[:success] = "正常に更新されました。"
+      redirect_to @user
+    else
+      flash.now[:danger] = "更新されませんでした。"
+      render :edit
+    end
+  end
+  
   private
   
   def user_params
