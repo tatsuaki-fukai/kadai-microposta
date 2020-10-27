2c5ae44d1cd07a7cb84cf2904dd62d7fe15612bd
aki <aki.egfree@gmail.com>
2020-10-27 22:22:23 +0000

    add favorites

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ApplicationRecord
   has_secure_password
   
   has_many :microposts
+  has_many :favorites
+  has_many :likes, through: :favorites, source: :micropost
   has_many :relationships
   has_many :followings, through: :relationships, source: :follow
   has_many :reverses_of_relationship, class_name: "Relationship", foreign_key: "follow_id"
@@ -29,5 +31,19 @@ class User < ApplicationRecord
   
   def feed_microposts
     Micropost.where(user_id: self.following_ids + [self.id])
+  end
+  
+  # お気に入り機能用のメソッド
+  def favorite(micropost)
+    self.favorites.find_or_create_by(micropost_id: micropost.id)
+  end
+  
+  def unfavorite(micropost)
+    favorite = self.favorites.find_by(micropost_id: micropost.id)
+    favorite.destroy if favorite
+  end
+  
+  def favorite?(other_micropost)
+    self.likes.include?(other_micropost)
   end
 end
