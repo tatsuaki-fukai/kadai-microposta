044dc409352ff0fa4ae444c68df026d6f8d3cb6a
aki <aki.egfree@gmail.com>
2020-10-28 10:36:39 +0000

    add before action followings followers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show, :likes]
+  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers, :likes]
   
   def index
     @users = User.order(id: :desc).page(params[:page]).per(25)
